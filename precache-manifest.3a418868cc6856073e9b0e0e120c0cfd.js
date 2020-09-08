781d2ee73572791beeeb37c86bf9d6c5848997bd
Adnan Birparic <adnan.birparic@gmail.com>
2020-09-08 07:21:17 +0200

    Updates

---
+++ precache-manifest.3a418868cc6856073e9b0e0e120c0cfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8616a9820d33714a58d1ff16d14bbb15",
+    "url": "/findmovie/index.html"
+  },
+  {
+    "revision": "924753ea4b60828ac4ea",
+    "url": "/findmovie/static/css/main.ec1d2532.chunk.css"
+  },
+  {
+    "revision": "f5511c19988dc79f8de6",
+    "url": "/findmovie/static/js/2.2e1a4706.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/findmovie/static/js/2.2e1a4706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924753ea4b60828ac4ea",
+    "url": "/findmovie/static/js/main.90b0230b.chunk.js"
+  },
+  {
+    "revision": "e1e1d4bf5761e514f985",
+    "url": "/findmovie/static/js/runtime-main.3a149ff7.js"
+  },
+  {
+    "revision": "2de4040a2cef94b1ba22a8ade9e817db",
+    "url": "/findmovie/static/media/cinema.2de4040a.png"
+  },
+  {
+    "revision": "878c7769f3435fc9f2625967b25129cf",
+    "url": "/findmovie/static/media/search.878c7769.png"
+  }
+]);
